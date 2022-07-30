3a7253b8517442e8ec06ada21806aa606df8b2e2
Surya Bhamidipati <suryanarayana.bhamidipati@siemens.com>
2022-07-30 06:43:25 +0530

    fill rect

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@
 DigitalOut led(LED1, 1);
 // Blinking rate in milliseconds
 #define BLINKING_RATE 500ms
-unsigned char pDest[9174];
+uint32_t pDest[64];
 
 void lubdub()
 {
@@ -35,22 +35,17 @@ int main()
     tmpreg = READ_BIT(RCC->AHB1ENR, RCC_AHB1ENR_DMA2DEN);
     printf("After enabling...DMA2D clock enable status...%x\n", tmpreg);
 
+    // Fill a rectangle
     // Set DMA2D config
-    //DMA2D->FGPFCCR = DMA2D_RGB565;
-   // DMA2D->FGCOLR = 0;
-    DMA2D->FGMAR = (uint32_t)stlogo;
-   // DMA2D->FGOR = 0;
-    DMA2D->CR = DMA2D_M2M;
-   // DMA2D->OPFCCR = DMA2D_RGB565;
-   // DMA2D->OCOLR = 0;
-    DMA2D->OMAR = (uint32_t)pDest;
-   // DMA2D->OOR = 0;
-    DMA2D->NLR = (uint32_t)((80 < 16) | 1);
-    
+    DMA2D->CR = 0x00030000UL; //Only fill a memory not copy
+    DMA2D->OMAR = (uint32_t)pDest; // Starting pixel address of rect
+    DMA2D->NLR = (uint32_t)(32<<16)|(uint16_t)2;//32X2 area
+    DMA2D->OOR = 0; // Don't skip any pixels
+    DMA2D->OPFCCR = 0; // ARGB8888
+    DMA2D->OCOLR = 0x00005a5a; //some 32 bit number
+    //Start filling
+    DMA2D->CR |= DMA2D_CR_START; 
 
-    printf("Pixel Count ..%x\n", (80<<16|1));
-    // Now enable a transfer
-    DMA2D->CR = DMA2D->CR | DMA2D_CR_START;
     while(DMA2D->CR & DMA2D_CR_START);
 
     
@@ -58,7 +53,8 @@ int main()
     printf("ISR status is ..%x and CR is ...%x\n", DMA2D->ISR, DMA2D->CR);
 
     printf("Data in Dest is ...%x %x %x\n", pDest[0], pDest[1], pDest[2] );
-    printf("Data in Source is ...%x %x %x\n", stlogo[0], stlogo[1], stlogo[2] );
+    //printf("Data in Source is ...%x %x %x\n", stlogo[0], stlogo[1], stlogo[2] );
+    
     while (1)
         ;
 }
